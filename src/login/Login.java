8e55a7b80e7b26f5b32a44262389648df95dc0ce
lihuimei0223 <915753113@qq.com>
2017-12-11 16:28:55 +0800

    fffffff

--- src/login/Login.java
+++ src/login/Login.java
@@ -4,11 +4,15 @@ import org.testng.annotations.Test;
 
 public class Login {
 
-	@Test
-	public void login(){
+	
+	public static void main(String args[]){
 		System.out.println("kkkkkkkkkkkkkkkkkkkkkk");
-		System.out.println("少时诵诗书所所所所所k");
-		System.out.println("ssss111111111111");
 	}
+//	@Test
+//	public void login(){
+//		System.out.println("kkkkkkkkkkkkkkkkkkkkkk");
+//		System.out.println("少时诵诗书所所所所所k");
+//		System.out.println("ssss111111111111");
+//	}
 	
 }
